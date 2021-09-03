041b7dcba0c7e0f7097bab79b3956942b1959c18
ismaelDembele <dembele.ismael@gmail.com>
2021-09-03 17:51:47 -0400

    test deployment

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ const store = new MongoDBStore({
   collection: "session",
 });
 
-// app.set("trust proxy", 1);
+app.set("trust proxy", 1);
 
 app.use(
   session({
@@ -55,6 +55,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       maxAge: 86400000, //1000*60*60*24 => 1day in milliseconds
+      secure: true,
     },
     store: store,
   })
