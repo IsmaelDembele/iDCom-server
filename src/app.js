b5e93e2f8ed53b17cbb0a5c37f3e362b099f93b9
ismaelDembele <dembele.ismael@gmail.com>
2021-10-18 08:06:56 -0400

    package updated

--- src/app.js
+++ src/app.js
@@ -28,8 +28,8 @@ const MONGO_URI = process.env.MONGO_URI;
 mongoose.connect(MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
+  // useCreateIndex: true,
+  // useFindAndModify: false,
 });
 
 const store = new MongoDBStore({
